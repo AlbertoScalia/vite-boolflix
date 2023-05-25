06a9b6e48b21a9b362efb330ec4919fd139ab75e
AlbertoScalia <albscalia@gmail.com>
2023-05-25 15:03:10 +0200

    First commit and API key

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+
+    MOVIE_API: 'https://api.themoviedb.org/3/movie/550?api_key=19d2c4e33ebd2ea5b31f34ef0140fe92',
+    query: null,
+})
