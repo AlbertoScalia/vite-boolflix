2bedf468f925af24c653eeb1a07b6d426fc03be1
AlbertoScalia <albscalia@gmail.com>
2023-05-28 15:04:57 +0200

    Feat: Add overview and searchbox style

--- src/store.js
+++ src/store.js
@@ -5,19 +5,22 @@ export const store = reactive({
   MOVIEDB_API:
     'https://api.themoviedb.org/3/search/multi?api_key=19d2c4e33ebd2ea5b31f34ef0140fe92&query=',
     MOVIEIMG_URL: "https://image.tmdb.org/t/p/",
-    query: null,
-    resultsList: [],
-    movieVote: null,
-    performSearchMovie(url) {
-      axios
-      .get(url)
-      .then((response) => {
-          //console.log(store.resultsList)
-          this.resultsList = response.data.results;
-          this.movieLanguage = this.resultsList.movieVote;
-      })
-      .catch(err => {
-          console.error(err)
-      });
-    },
-  });
+    query: '',
+  resultsList: [],
+  movieVote: null,
+  loading: true,
+  performSearchMovie(url) {
+    axios
+    .get(url)
+    .then((response) => {
+        //console.log(store.resultsList)
+        this.resultsList = response.data.results;
+        this.movieLanguage = this.resultsList.movieVote;
+
+        this.loading = false;
+    })
+    .catch(err => {
+        console.error(err)
+    });
+  },
+});
